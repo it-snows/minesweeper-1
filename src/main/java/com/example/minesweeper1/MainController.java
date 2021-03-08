e18d999e90e2412fa236cd38796989eca8e6363c
it-snows <s.sniedzina@gmail.com>
2021-03-08 20:41:25 +0200

    final without flags

--- src/main/java/com/example/minesweeper1/MainController.java
+++ src/main/java/com/example/minesweeper1/MainController.java
@@ -2,21 +2,29 @@ package com.example.minesweeper1;
 
 import com.example.minesweeper1.data.Board;
 import com.example.minesweeper1.data.Game;
+import com.example.minesweeper1.data.GameResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
     @GetMapping("")
-    public String getIndexPage(Model model) {
+    public String getIndexPage(Model model, @RequestParam(required = false) Integer fieldSize) {
 
-        if(Minesweeper1Application.game == null){
-            Minesweeper1Application.game = new Game (6);
+        var gameStr = "";
+
+        if (fieldSize != null) {
+            if (Minesweeper1Application.game == null) {
+                Minesweeper1Application.game = new Game(fieldSize);
+            }
+            gameStr = getGameHtml(Minesweeper1Application.game);
         }
 
-        model.addAttribute("game", getGameHtml(Minesweeper1Application.game));
+        model.addAttribute("game", gameStr);
+        model.addAttribute("status", GameResult.CONTINUE.toString());
 
         return "index";
     }
@@ -24,15 +32,25 @@ public class MainController {
     @GetMapping("/check")
     public String checkField(Model model, @RequestParam int row, @RequestParam int column) {
 
-        if(Minesweeper1Application.game == null){
-            Minesweeper1Application.game = new Game (6);
+        if (Minesweeper1Application.game == null) {
+            Minesweeper1Application.game = new Game(6);
         }
 
-        Minesweeper1Application.game.checkField(row, column);
+        var result = Minesweeper1Application.game.checkField(row, column);
 
+        model.addAttribute("status", result.toString());
         model.addAttribute("game", getGameHtml(Minesweeper1Application.game));
 
+
         return "index";
+    }
+
+    @GetMapping("/newgame")
+    public ModelAndView newGame() {
+
+        Minesweeper1Application.game = null;
+
+        return new ModelAndView("redirect:/");
     }
 
     private String getGameHtml(Game game) {
@@ -62,7 +80,15 @@ public class MainController {
                 return "<div class=\"field-empty\">&nbsp;</div>";
             }
             case Board.FIELD_CLOSED: {
-                return "<a class=\"field-closed\" href=\"/check?row=" + row + "&column=" + column + "\">&nbsp;</a>";
+
+                if (Minesweeper1Application.game.isGameFinished()) {
+
+                    return "<div class=\"field-closed\"></div>";
+
+                } else {
+
+                    return "<a class=\"field-closed\" href=\"/check?row=" + row + "&column=" + column + "\">&nbsp;</a>";
+                }
             }
         }
         return "<div class=\"field-number\"><span class=\"number\">" + i + "</span></div>";
--- src/main/java/com/example/minesweeper1/data/Game.java
+++ src/main/java/com/example/minesweeper1/data/Game.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 public class Game {
     private Board board;
 
+    private GameResult currentGameStatus;
+
     @Getter
     private final int size;
 
@@ -21,11 +23,13 @@ public class Game {
     public GameResult checkField(int row, int column) {
         var result = board.checkField(row, column);
 
-        if(!result) {
+        if (!result) {
+            currentGameStatus = GameResult.LOST;
             return GameResult.LOST;
         }
 
-        if(board.isOpen()) {
+        if (board.isOpen()) {
+            currentGameStatus = GameResult.WON;
             return GameResult.WON;
         }
 
@@ -34,5 +38,9 @@ public class Game {
 
     public int getFieldValue(int row, int column) {
         return board.getFieldInfo(row, column);
+    }
+
+    public boolean isGameFinished() {
+        return currentGameStatus == GameResult.LOST || currentGameStatus == GameResult.WON;
     }
 }
